a2c1770cb83715bd618aaad52798a506c4a57eb6
octaprog7 <goctaprog@gmail.com>
2022-10-09 16:02:30 +0500

    ...

--- converter.py
+++ converter.py
@@ -1,5 +1,5 @@
 """Преобразование значений из одной единицы измерения в другую.
-Сonverting values from one unit of measure to another"""
+Converting values from one unit of measure to another"""
 
 
 def pa_mmhg(value: float) -> float:
